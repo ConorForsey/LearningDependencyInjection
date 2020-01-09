a9d1d965a29a948e8bbe5e755aff6241265e6a6e
ConorForsey <conorforsey@yahoo.co.uk>
2020-01-09 15:15:09 +0000

    Add files via upload

---
+++ LearningDependencyInjection/LearningDependencyInjection/EmployeeBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDependencyInjection
+{
+    public class EmployeeBL //Employee base layer
+    {
+        public IEmployeeDAL employeeDAL;
+
+        public EmployeeBL(IEmployeeDAL employeeDAL)
+        {
+            this.employeeDAL = employeeDAL;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeDAL.GetAllEmployees();
+        }
+    }
+}
---
+++ LearningDependencyInjection/LearningDependencyInjection/EmployeeDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDependencyInjection
+{
+    public interface IEmployeeDAL
+    {
+        List<Employee> GetAllEmployees();
+    }
+
+    public class EmployeeDAL : IEmployeeDAL //Employee data access layer
+    {
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+            listEmployees.Add(new Employee() { ID = 1, Name = "Conor",  Department = "IT"});
+            listEmployees.Add(new Employee() { ID = 2, Name = "Alex",   Department = "Payroll" });
+            listEmployees.Add(new Employee() { ID = 3, Name = "Karina", Department = "HR" });
+
+            return listEmployees;
+        }
+    }
+}
